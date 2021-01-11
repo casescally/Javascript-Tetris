61936c2adf19330e32511cd271e57f6cec484682
Case Scally <caseScally@gmail.com>
2021-01-11 17:54:37 -0600

    app setup

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    //code goes here
-    
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  //take all the divs in the grid and pass them through the Array.from method, putting them into an array
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartBtn = document.querySelector("#start-button");
+  const width = 10;
+
+  console.log(squares);
+});
