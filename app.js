25855dae97c166de3f7976b97a5f1d1e63ece4a5
Case Scally <caseScally@gmail.com>
2021-01-11 19:14:15 -0600

    added move down and freeze function

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
-    [1, withd, width + 1, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
   ];
 
   const oTetromino = [
@@ -49,4 +49,52 @@ document.addEventListener("DOMContentLoaded", () => {
     oTetromino,
     iTetromino,
   ];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+  //ramdomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][0];
+
+  //draw the first rotation in the first tetromino
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("tetromino");
+    });
+  }
+
+  function undraw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("tetromino");
+    });
+  }
+
+  //make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000);
+
+  //move down functionn
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  //freeze function
+  function freeze() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains("taken")
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      //start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
 });
